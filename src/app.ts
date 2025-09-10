ad48904d57d8862aa54df5f412acb3ca7d0dd03c
Alejandro Toro <alejotoro94@hotmail.com>
2025-09-10 15:38:18 -0500

    Subo configuración inicial de la API

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { Server } from "./presentation/server";
+
+( async() => {
+
+    await main();
+
+})()
+
+
+async function main() {
+    const server = new Server();
+    server.start();
+    
+}
+
